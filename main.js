e4ee0236b9ca02ec8ac8d4f592ca33d328e37ec2
Dragon Master <73437981+ReynaldoSoto@users.noreply.github.com>
2021-05-16 15:28:17 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristY = 0;
+leftWristX = 0;
+rightWristY = 0;
+rightWristX = 0;
 
 function preload(){
     song1 = loadSound("Marshmello - Happier (Official).mp3");
@@ -12,8 +16,29 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Intialized");
 }
 
 function draw(){
     image(video, 0, 0, 650, 500);
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightwristX = " + rightWristX + "rightwristY = " + rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftwristX = " + leftWristX + "leftwristY = " + leftWristY);
+    }
 }
